235f965a0f11fd582237b3e3b00951b6632bc43e
Prabharsha <pansiluprabharsha1@gamil.com>
2022-06-22 18:04:55 +0530

    updated

---
+++ 02_HttpRequest/src/TestDeploymentServlet.java
@@ -0,0 +1,15 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class TestDeploymentServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Request Received");
+        PrintWriter writer = resp.getWriter();
+        writer.write("<h1 style=\"color:red\"> Response Generated</h1>");
+    }
+}
