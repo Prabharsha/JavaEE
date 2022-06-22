566ee987ad80b69169110f49c6e595c85f35ff71
Prabharsha <pansiluprabharsha1@gamil.com>
2022-06-22 23:51:03 +0530

    updated mapping specs

--- MappingSpec_03/src/WildCardMapping.java
+++ MappingSpec_03/src/WildCardMapping.java
@@ -10,5 +10,15 @@ public class WildCardMapping extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Wild Card Mapping Spec Invoked");
+        String method = req.getMethod();
+        String servletPath = req.getServletPath();
+        String contextPath = req.getContextPath();
+        String pathInfo = req.getPathInfo();
+
+        System.out.println("Method " +method);
+        System.out.println("ServletPath "+servletPath);
+        System.out.println("contextPath "+contextPath);
+        System.out.println("Path Info "+pathInfo);
+
     }
 }
