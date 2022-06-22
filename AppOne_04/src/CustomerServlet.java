60b1220d24451a94fa6833d4c735d12f1fe7861e
Prabharsha <pansiluprabharsha1@gamil.com>
2022-06-23 01:56:07 +0530

    ApOne Created and done

---
+++ AppOne_04/src/CustomerServlet.java
@@ -0,0 +1,43 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@WebServlet(urlPatterns = "/customer")
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/company","root","1234");
+            ResultSet rst = connection.prepareStatement("SELECT * FROM customer").executeQuery();
+            String records="";
+            while (rst.next()){
+                String id = rst.getString(1);
+                String name = rst.getString(2);
+                String address = rst.getString(3);
+                double salary = rst.getDouble(4);
+                System.out.println(id+" "+name+" "+address+" "+salary);
+
+                //convert one record to Json
+                String customer = "{\"id\":\""+id+"\",\"name\":\""+name+"\",\"address\":\""+address+"\",\"salary\":"+salary+"},";
+                records=records+customer;
+            }
+            String finalJson ="[" + records.substring(0,records.length()-1) + "]";
+            PrintWriter writer = resp.getWriter();
+            writer.write(finalJson);
+        } catch (ClassNotFoundException | SQLException e) {
+           e.printStackTrace();
+        }
+
+
+    }
+}
